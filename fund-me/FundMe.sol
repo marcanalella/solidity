53d0e2a39aabda5d7e8ec974e1ef15b0418efb8b
marcanalella <marcanalella@gmail.com>
2024-11-30 14:07:22 +0100

    add fund function

--- fund-me/FundMe.sol
+++ fund-me/FundMe.sol
@@ -2,7 +2,23 @@
 pragma solidity ^0.8.18;
 
 contract FundMe {
-    function fund() public {}
+
+    uint256 public minimumUsd = 5;
+
+    uint256 public minimumEur = 4;
+
+    //payable make possible to take fund to the contract like a wallet
+    function fund() public payable {
+        // https://eth-converter.com/
+        require(msg.value > 1e18, "didn't send enought ETH");//1e18 = 1 ETH = 10000000000000000000 Wei
+        //What is a revert?
+        // Undo any actions that have been done, and send the remaining gas back
+        //A failed transaction spend gas becaouse computer executed task.
+        // if a transaction reverts, it undoes anything it does previously and
+        //you can consider the transaction failed.
+        //if you send a reverted transaction, you will spend gas.
     
-    function withdraw() public {}
+    }
+    
+    //function withdraw() public {}
 }
