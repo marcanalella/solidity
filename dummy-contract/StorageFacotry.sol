e6663993bb786fac32828da34426b84cdd7436ec
marcanalella <marcanalella@gmail.com>
2024-11-30 11:17:59 +0100

    add call to another contract

--- dummy-contract/StorageFacotry.sol
+++ dummy-contract/StorageFacotry.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 //import "dummy-contract/SimpleStorage.sol"; not use this but named imports
-import {SimpleStorage} from "dummy-contract/SimpleStorage.sol"; //not use this but named imports
+import {SimpleStorage} from "./SimpleStorage.sol"; //not use this but named imports
 
 contract StorageFactory {
     SimpleStorage[] public simpleStorageList;
