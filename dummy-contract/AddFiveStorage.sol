54b52d161170530c6a6ca85068e0fd6acbc01879
marcanalella <marcanalella@gmail.com>
2025-06-30 19:58:53 +0200

    add comment

--- dummy-contract/AddFiveStorage.sol
+++ dummy-contract/AddFiveStorage.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.18;
 
 import {SimpleStorage} from "./SimpleStorage.sol";
 
+//inheritance and overriding in Solidity
+
 contract AddFiveStorage is SimpleStorage {
     //it's not a view because we aren't reading from storage
     function sayHello() public pure returns (string memory) {
